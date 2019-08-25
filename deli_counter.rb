4ac414e2c5fbc98bb4e040750f12f95d31ff92e2
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-25 21:50:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,49 @@
-def line(array)
-  if array.length > 0
-    line_list = []
-    array.each_with_index do |name,index|
-      line_list << ("#{index+1}. #{name}")
-    end 
-    puts "The line is currently: #{line_list.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
-end
-
-def take_a_number(array,string)
-  array << string
-  puts "Welcome, " + "#{string}. You are number #{array.count} in line."
-end
 
 
-def now_serving(array)
-  if array.length == 0
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{array[0]}."
-    array.shift
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def line(array)
+#   if array.length > 0
+#     line_list = []
+#     array.each_with_index do |name,index|
+#       line_list << ("#{index+1}. #{name}")
+#     end 
+#     puts "The line is currently: #{line_list.join(" ")}"
+#   else
+#     puts "The line is currently empty."
+#   end
+# end
+
+# def take_a_number(array,string)
+#   array << string
+#   puts "Welcome, " + "#{string}. You are number #{array.count} in line."
+# end
+
+
+# def now_serving(array)
+#   if array.length == 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{array[0]}."
+#     array.shift
+#   end
+# end
